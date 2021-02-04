b921fc98575af87e1744882ef393f5b3ec2ee0ca
Dan <github@aerolith.net>
2021-02-04 10:39:14 -0500

    inital import

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dlasky/gotk3-layershell
+
+go 1.15
+
+require github.com/gotk3/gotk3 v0.5.2
---
+++ layershell/layer.go
@@ -0,0 +1,54 @@
+package layershell
+
+// #cgo pkg-config: gtk+-3.0 gtk-layer-shell-0
+// #include <gtk/gtk.h>
+// #include "gtk-layer-shell.h"
+import "C"
+import (
+	"github.com/gotk3/gotk3/gtk"
+	"unsafe"
+)
+
+type LayerShellEdgeFlags int
+
+const (
+	LAYER_SHELL_EDGE_LEFT   LayerShellEdgeFlags = iota
+	LAYER_SHELL_EDGE_RIGHT
+	LAYER_SHELL_EDGE_TOP
+	LAYER_SHELL_EDGE_BOTTOM
+)
+
+type LayerShellLayerFlags int
+
+const (
+	LAYER_SHELL_LAYER_BACKGROUND LayerShellLayerFlags = iota
+	LAYER_SHELL_LAYER_BOTTOM
+	LAYER_SHELL_LAYER_TOP
+	LAYER_SHELL_LAYER_OVERLAY    
+)
+
+func native(window *gtk.Window) *C.GtkWindow {
+	w := window.Native()
+	wp := (*C.GtkWindow)(unsafe.Pointer(w))
+	return wp;
+}
+
+func InitForWindow(window *gtk.Window) {
+	w := native(window);
+	C.gtk_layer_init_for_window(w)
+}
+
+func SetLayer(window *gtk.Window, layer LayerShellLayerFlags) {
+	w := native(window)
+	C.gtk_layer_set_layer(w, C.GtkLayerShellLayer(layer))
+}
+
+func AutoExclusiveZoneEnable(window *gtk.Window) {
+	w := native(window)
+	C.gtk_layer_auto_exclusive_zone_enable(w)
+}
+
+func SetMargin(window *gtk.Window, side LayerShellEdgeFlags, margin int) {
+	w := native(window)
+	C.gtk_layer_set_margin(w, C.GtkLayerShellEdge(side), C.int(margin))
+}
